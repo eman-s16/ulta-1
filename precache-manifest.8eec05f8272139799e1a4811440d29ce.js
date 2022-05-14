7f6b308ccd3676a8da60d65673c59593b6708d15
eman-s16 <projectm812@gmail.com>
2022-05-14 22:26:08 +0400

    Updates

---
+++ precache-manifest.8eec05f8272139799e1a4811440d29ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ac399f7b8393ee8cc159",
+    "url": "/ulta-1/static/css/main.f9db860d.chunk.css"
+  },
+  {
+    "revision": "ac399f7b8393ee8cc159",
+    "url": "/ulta-1/static/js/main.ac399f7b.chunk.js"
+  },
+  {
+    "revision": "dafd8c5947db5a4e6fff",
+    "url": "/ulta-1/static/js/1.dafd8c59.chunk.js"
+  },
+  {
+    "revision": "eaa71a671c8432ddd4ff",
+    "url": "/ulta-1/static/js/runtime~main.eaa71a67.js"
+  },
+  {
+    "revision": "64649de3cb8db5e02994d5f678d74fce",
+    "url": "/ulta-1/index.html"
+  }
+];
